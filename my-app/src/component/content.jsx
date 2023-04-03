a2640c8e984c324d2051da12ba2c0e2ded2c1605
Mikhael Wellman <121779772+mikhael-wellman510@users.noreply.github.com>
2023-04-03 10:05:10 +0700

    navbar

--- my-app/src/component/content.jsx
+++ my-app/src/component/content.jsx
@@ -2,258 +2,256 @@ import "../css/spotify.css";
 import playbuttonB from "./assetB/greenplaybuttonB.png";
 
 export default function Content() {
-	const data1 = [
-		{
-			url: "https://i.scdn.co/image/ab6765630000ba8ae5dc92dfde95167046b65d52",
-			tittle: "Do You See What I See?",
-			keterangan: "INDONESIAN HORROR STORIES",
-		},
-		{
-			url: "https://i.scdn.co/image/bf70816654fc829e007bc0e2f9abf789406c8c8f",
-			tittle: "MENDOAN",
-			keterangan: "DONOTIAN",
-		},
-		{
-			url: "https://i.scdn.co/image/ab67656300005f1f5709856aaf963cce726eaa48",
-			tittle: "Musuh Masyarakat",
-			keterangan: "NOICE x NEXT Studios",
-		},
-		{
-			url: "https://i.scdn.co/image/ab67656300005f1f08d799296a0a977137a65ede",
-			tittle: "GJLS",
-			keterangan: "Podkesmas Asia",
-		},
-		{
-			url: "https://i.scdn.co/image/44ba2e9e175c805c1bba657a8ecf6b9c26b3dbf5",
-			tittle: "Rapot",
-			keterangan: "RezaAnkaRadhiniAbigail Potkes",
-		},
-		{
-			url: "https://i.scdn.co/image/ab67656300005f1fff96c93a3417474d446fd7d4",
-			tittle: "Podcast Seminggu",
-			keterangan: "Podkesmas Asia",
-		},
-		{
-			url: "https://i.scdn.co/image/ab67656300005f1f2a2cde63bc0f92df1f1e059e",
-			tittle: "PODCAST ANCUR",
-			keterangan: "Patra, Diaz, Kemal, Dhika",
-		},
-		{
-			url: "https://i.scdn.co/image/ab67656300005f1f756c2a41f37ce4fb20d47485",
-			tittle: "To Fluency Podcast: English with Jack",
-			keterangan: "JDA Industries Inc.",
-		},
-		{
-			url: "https://i.scdn.co/image/ab67656300005f1fd0393656bbd0013a88e58e2a",
-			tittle: "Pengembangan Diri",
-			keterangan: "Andreas Bordes x Box2BoxID",
-		},
-	];
-	const data2 = [
-		{
-			url: "https://i.scdn.co/image/ab67706f000000032ce135435aed1c646d9d0c0e",
-			tittle: "Hot Hits Indonesia",
-			keterangan:
-				"Hits terpanas dari SIA, Mahalini, serta artis terfavorit Anda dari dalam negeri dan mancanegara.",
-		},
-		{
-			url: "https://i.scdn.co/image/ab67616d0000b27335f1e6eb48870a8e15620570",
-			tittle: "Runtuh",
-			keterangan: "2021. Febby Putri, Fiersa Besari",
-		},
-		{
-			url: "https://seeded-session-images.scdn.co/v1/img/artist/4Z9pg3LzMfuuSgl3hMDYuG/en",
-			tittle: "Diskoria Radio",
-			keterangan: "By Spotify",
-		},
-		{
-			url: "https://thisis-images.scdn.co/37i9dQZF1DZ06evO2cV1FC-large.jpg",
-			tittle: "This is Joji",
-			keterangan: "By Spotify",
-		},
-		{
-			url: "https://fitacg.com/media/article/img/5b/fdbc7bb06003fb71.jpg",
-			tittle: `"Suzume" & Songs`,
-			keterangan: "By Spotify",
-		},
-		{
-			url: "https://i.scdn.co/image/ab67706c0000da8431e6907077eea65f2a2b939e",
-			tittle: "was here",
-			keterangan: "places that told stories.",
-		},
-		{
-			url: "https://i.scdn.co/image/ab67706f00000003d57ef80dbfc625b52aa3b15f",
-			tittle: "Flashback",
-			keterangan: "Throwback to Indonesian pop songs.",
-		},
-		{
-			url: "https://i.scdn.co/image/ab67706f000000037b14a53e36eef03ba4ba5749",
-			tittle: "Terbaik 2000an",
-			keterangan: "Lagu Indonesia kesayangan kamu sepanjang era 2000an.",
-		},
-		{
-			url: "https://i.scdn.co/image/ab67706c0000da84ee0e6ff23ac5c059594f03a4",
-			tittle: "J",
-			keterangan: "Jordan Ong",
-		},
-	];
-	const data3 = [
-		{
-			url: "https://charts-images.scdn.co/assets/locale_en/regional/weekly/region_global_default.jpg",
-			tittle: `Top Songs - Global `,
-			keterangan:
-				"Your weekly update of the most played tracks right now - Global.",
-		},
-		{
-			url: "https://th.bing.com/th/id/OIP.6rAMU2oF3SF47ZcD9_ezKgHaHa?w=173&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-			tittle: `Top-50 Global `,
-			keterangan:
-				"Your daily update of the most played tracks right now - Global.",
-		},
-		{
-			url: "https://charts-images.scdn.co/assets/locale_en/regional/daily/region_id_default.jpg",
-			tittle: `Top-50 Indonesia `,
-			keterangan:
-				"Your daily update of the most played tracks right now - Indonesia.",
-		},
-		{
-			url: "https://www.kolibrimusic.com/wp-content/uploads/2018/03/spotify-viral-50-global-1024x1024.jpg",
-			tittle: `Viral-50 Global`,
-			keterangan:
-				"Your daily update of the most viral tracks right now - Global",
-		},
-		{
-			url: "https://charts-images.scdn.co/assets/locale_en/viral/daily/region_id_default.jpg",
-			tittle: `Viral-50 Indonesia`,
-			keterangan:
-				"Your daily update of the most viral tracks right now - Indonesia.",
-		},
-	];
-	const data4 = [
-		{
-			url: "https://i.scdn.co/image/ab6765630000ba8a9306a058753d9fbf5a055ff3",
-			tittle: "Gema Membiru",
-			keterangan: "Firdhani Zihan & Kukila",
-		},
-		{
-			url: "https://th.bing.com/th/id/OIP.OZ8WxrsCmY7NB6vV67wr3QAAAA?pid=ImgDet&rs=1",
-			tittle: "Ramalan Zodiac Hari Ini",
-			keterangan: "Spotify Studios & Parcast",
-		},
-		{
-			url: "https://i.scdn.co/image/ab67656300005f1fc7c40b8d5709f903847038f5",
-			tittle: "Festival Patah Hati",
-			keterangan: "Spotify Studios & Kukila",
-		},
-		{
-			url: "https://th.bing.com/th/id/OIP.J_-XyfeyndF4OK9f0Rm3agHaHa?w=197&h=197&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-			tittle: "PODKESMAS",
-			keterangan: "Podkesmas Asia",
-		},
-		{
-			url: "https://i.scdn.co/image/779396289ed64478d3d54ec5eb7cbc4eaa952dd2",
-			tittle: "Podcat Malam Kliwon",
-			keterangan: "Podkesmas Asia",
-		},
-		{
-			url: "https://th.bing.com/th/id/OIP.StnvMkOtl493BQDVdAMN9gHaHa?w=197&h=197&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-			tittle: "AGAK LAEN!",
-			keterangan: "Agak Laen",
-		},
-		{
-			url: "https://th.bing.com/th/id/OIP.62Jv2ZYtgib8KsnOxVeRoQHaHa?w=179&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-			tittle: "Ada Hantu di Sekolah",
-			keterangan: "Spotify Studios & Kukila",
-		},
-		{
-			url: "https://th.bing.com/th/id/OIP.inV1i4UHXUAgHOx7Uni8HQHaHa?w=185&h=185&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-			tittle: "Rintik Sedu",
-			keterangan: "Rintiksedu",
-		},
-		{
-			url: "https://th.bing.com/th/id/OIP.tpWJiPOKJ2dermp_s6qgvQAAAA?pid=ImgDet&rs=1",
-			tittle: "DESTAnya SIAPA?",
-			keterangan: "Deddy Mahendra Desta",
-		},
-	];
-	return (
-		<div className="backgroundB">
-			<div className="paddingKontenB">
-				<div className="kontenerB">
-					<div className="namaGroupB">
-						<div className="h1B">
-							Spotify original & exlusive shows
-						</div>
-						<div className="h2B">Show all</div>
-					</div>
-					<div id="musicCoverB">
-						{data1.map((val) => (
-							<CardB
-								url={val.url}
-								url2={val.url2}
-								tittle={val.tittle}
-								keterangan={val.keterangan}
-							/>
-						))}
-					</div>
-				</div>
-				<div className="kontenerB">
-					<div className="namaGroupB">
-						<div className="h1B">Try something else</div>
-						<div className="h2B">Show all</div>
-					</div>
-					<div id="musicCoverB">
-						{data2.map((val) => (
-							<CardB
-								url={val.url}
-								url2={val.url2}
-								tittle={val.tittle}
-								keterangan={val.keterangan}
-							/>
-						))}
-					</div>
-				</div>
-				<div className="kontenerB">
-					<div className="namaGroupB">
-						<div className="h1B">Featured Charts</div>
-						<div className="h2B">Show all</div>
-					</div>
-					<div id="musicCoverB">
-						{data3.map((val) => (
-							<CardB
-								url={val.url}
-								url2={val.url2}
-								tittle={val.tittle}
-								keterangan={val.keterangan}
-							/>
-						))}
-					</div>
-				</div>
-				<div className="kontenerB">
-					<div className="namaGroupB">
-						<div className="h1B">Shows to try</div>
-						<div className="h2B">Show all</div>
-					</div>
-					<div id="musicCoverB">
-						{data4.map((val) => (
-							<CardB
-								url={val.url}
-								url2={val.url2}
-								tittle={val.tittle}
-								keterangan={val.keterangan}
-							/>
-						))}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  const data1 = [
+    {
+      url: "https://i.scdn.co/image/ab6765630000ba8ae5dc92dfde95167046b65d52",
+      tittle: "Do You See What I See?",
+      keterangan: "INDONESIAN HORROR STORIES",
+    },
+    {
+      url: "https://i.scdn.co/image/bf70816654fc829e007bc0e2f9abf789406c8c8f",
+      tittle: "MENDOAN",
+      keterangan: "DONOTIAN",
+    },
+    {
+      url: "https://i.scdn.co/image/ab67656300005f1f5709856aaf963cce726eaa48",
+      tittle: "Musuh Masyarakat",
+      keterangan: "NOICE x NEXT Studios",
+    },
+    {
+      url: "https://i.scdn.co/image/ab67656300005f1f08d799296a0a977137a65ede",
+      tittle: "GJLS",
+      keterangan: "Podkesmas Asia",
+    },
+    {
+      url: "https://i.scdn.co/image/44ba2e9e175c805c1bba657a8ecf6b9c26b3dbf5",
+      tittle: "Rapot",
+      keterangan: "RezaAnkaRadhiniAbigail Potkes",
+    },
+    {
+      url: "https://i.scdn.co/image/ab67656300005f1fff96c93a3417474d446fd7d4",
+      tittle: "Podcast Seminggu",
+      keterangan: "Podkesmas Asia",
+    },
+    {
+      url: "https://i.scdn.co/image/ab67656300005f1f2a2cde63bc0f92df1f1e059e",
+      tittle: "PODCAST ANCUR",
+      keterangan: "Patra, Diaz, Kemal, Dhika",
+    },
+    {
+      url: "https://i.scdn.co/image/ab67656300005f1f756c2a41f37ce4fb20d47485",
+      tittle: "To Fluency Podcast: English with Jack",
+      keterangan: "JDA Industries Inc.",
+    },
+    {
+      url: "https://i.scdn.co/image/ab67656300005f1fd0393656bbd0013a88e58e2a",
+      tittle: "Pengembangan Diri",
+      keterangan: "Andreas Bordes x Box2BoxID",
+    },
+  ];
+  const data2 = [
+    {
+      url: "https://i.scdn.co/image/ab67706f000000032ce135435aed1c646d9d0c0e",
+      tittle: "Hot Hits Indonesia",
+      keterangan:
+        "Hits terpanas dari SIA, Mahalini, serta artis terfavorit Anda dari dalam negeri dan mancanegara.",
+    },
+    {
+      url: "https://i.scdn.co/image/ab67616d0000b27335f1e6eb48870a8e15620570",
+      tittle: "Runtuh",
+      keterangan: "2021. Febby Putri, Fiersa Besari",
+    },
+    {
+      url: "https://seeded-session-images.scdn.co/v1/img/artist/4Z9pg3LzMfuuSgl3hMDYuG/en",
+      tittle: "Diskoria Radio",
+      keterangan: "By Spotify",
+    },
+    {
+      url: "https://thisis-images.scdn.co/37i9dQZF1DZ06evO2cV1FC-large.jpg",
+      tittle: "This is Joji",
+      keterangan: "By Spotify",
+    },
+    {
+      url: "https://fitacg.com/media/article/img/5b/fdbc7bb06003fb71.jpg",
+      tittle: `"Suzume" & Songs`,
+      keterangan: "By Spotify",
+    },
+    {
+      url: "https://i.scdn.co/image/ab67706c0000da8431e6907077eea65f2a2b939e",
+      tittle: "was here",
+      keterangan: "places that told stories.",
+    },
+    {
+      url: "https://i.scdn.co/image/ab67706f00000003d57ef80dbfc625b52aa3b15f",
+      tittle: "Flashback",
+      keterangan: "Throwback to Indonesian pop songs.",
+    },
+    {
+      url: "https://i.scdn.co/image/ab67706f000000037b14a53e36eef03ba4ba5749",
+      tittle: "Terbaik 2000an",
+      keterangan: "Lagu Indonesia kesayangan kamu sepanjang era 2000an.",
+    },
+    {
+      url: "https://i.scdn.co/image/ab67706c0000da84ee0e6ff23ac5c059594f03a4",
+      tittle: "J",
+      keterangan: "Jordan Ong",
+    },
+  ];
+  const data3 = [
+    {
+      url: "https://charts-images.scdn.co/assets/locale_en/regional/weekly/region_global_default.jpg",
+      tittle: `Top Songs - Global `,
+      keterangan:
+        "Your weekly update of the most played tracks right now - Global.",
+    },
+    {
+      url: "https://th.bing.com/th/id/OIP.6rAMU2oF3SF47ZcD9_ezKgHaHa?w=173&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+      tittle: `Top-50 Global `,
+      keterangan:
+        "Your daily update of the most played tracks right now - Global.",
+    },
+    {
+      url: "https://charts-images.scdn.co/assets/locale_en/regional/daily/region_id_default.jpg",
+      tittle: `Top-50 Indonesia `,
+      keterangan:
+        "Your daily update of the most played tracks right now - Indonesia.",
+    },
+    {
+      url: "https://www.kolibrimusic.com/wp-content/uploads/2018/03/spotify-viral-50-global-1024x1024.jpg",
+      tittle: `Viral-50 Global`,
+      keterangan:
+        "Your daily update of the most viral tracks right now - Global",
+    },
+    {
+      url: "https://charts-images.scdn.co/assets/locale_en/viral/daily/region_id_default.jpg",
+      tittle: `Viral-50 Indonesia`,
+      keterangan:
+        "Your daily update of the most viral tracks right now - Indonesia.",
+    },
+  ];
+  const data4 = [
+    {
+      url: "https://i.scdn.co/image/ab6765630000ba8a9306a058753d9fbf5a055ff3",
+      tittle: "Gema Membiru",
+      keterangan: "Firdhani Zihan & Kukila",
+    },
+    {
+      url: "https://th.bing.com/th/id/OIP.OZ8WxrsCmY7NB6vV67wr3QAAAA?pid=ImgDet&rs=1",
+      tittle: "Ramalan Zodiac Hari Ini",
+      keterangan: "Spotify Studios & Parcast",
+    },
+    {
+      url: "https://i.scdn.co/image/ab67656300005f1fc7c40b8d5709f903847038f5",
+      tittle: "Festival Patah Hati",
+      keterangan: "Spotify Studios & Kukila",
+    },
+    {
+      url: "https://th.bing.com/th/id/OIP.J_-XyfeyndF4OK9f0Rm3agHaHa?w=197&h=197&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+      tittle: "PODKESMAS",
+      keterangan: "Podkesmas Asia",
+    },
+    {
+      url: "https://i.scdn.co/image/779396289ed64478d3d54ec5eb7cbc4eaa952dd2",
+      tittle: "Podcat Malam Kliwon",
+      keterangan: "Podkesmas Asia",
+    },
+    {
+      url: "https://th.bing.com/th/id/OIP.StnvMkOtl493BQDVdAMN9gHaHa?w=197&h=197&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+      tittle: "AGAK LAEN!",
+      keterangan: "Agak Laen",
+    },
+    {
+      url: "https://th.bing.com/th/id/OIP.62Jv2ZYtgib8KsnOxVeRoQHaHa?w=179&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+      tittle: "Ada Hantu di Sekolah",
+      keterangan: "Spotify Studios & Kukila",
+    },
+    {
+      url: "https://th.bing.com/th/id/OIP.inV1i4UHXUAgHOx7Uni8HQHaHa?w=185&h=185&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+      tittle: "Rintik Sedu",
+      keterangan: "Rintiksedu",
+    },
+    {
+      url: "https://th.bing.com/th/id/OIP.tpWJiPOKJ2dermp_s6qgvQAAAA?pid=ImgDet&rs=1",
+      tittle: "DESTAnya SIAPA?",
+      keterangan: "Deddy Mahendra Desta",
+    },
+  ];
+  return (
+    <div className="backgroundB">
+      <div className="paddingKontenB">
+        <div className="kontenerB">
+          <div className="namaGroupB">
+            <div className="h1B">Spotify original & exlusive shows</div>
+            <div className="h2B">Show all</div>
+          </div>
+          <div id="musicCoverB">
+            {data1.map((val) => (
+              <CardB
+                url={val.url}
+                url2={val.url2}
+                tittle={val.tittle}
+                keterangan={val.keterangan}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="kontenerB">
+          <div className="namaGroupB">
+            <div className="h1B">Try something else</div>
+            <div className="h2B">Show all</div>
+          </div>
+          <div id="musicCoverB">
+            {data2.map((val) => (
+              <CardB
+                url={val.url}
+                url2={val.url2}
+                tittle={val.tittle}
+                keterangan={val.keterangan}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="kontenerB">
+          <div className="namaGroupB">
+            <div className="h1B">Featured Charts</div>
+            <div className="h2B">Show all</div>
+          </div>
+          <div id="musicCoverB">
+            {data3.map((val) => (
+              <CardB
+                url={val.url}
+                url2={val.url2}
+                tittle={val.tittle}
+                keterangan={val.keterangan}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="kontenerB">
+          <div className="namaGroupB">
+            <div className="h1B">Shows to try</div>
+            <div className="h2B">Show all</div>
+          </div>
+          <div id="musicCoverB">
+            {data4.map((val) => (
+              <CardB
+                url={val.url}
+                url2={val.url2}
+                tittle={val.tittle}
+                keterangan={val.keterangan}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function CardB(props) {
-	return (
-		<div className="cardB">
-			{/* <div className="playbuttonB">
+  return (
+    <div className="cardB">
+      {/* <div className="playbuttonB">
 				<img
 					id="playbuttonB"
 					src={playbuttonB}
@@ -262,28 +260,28 @@ function CardB(props) {
 					height={"70px"}
 				/>
 			</div> */}
-			<div className="gambarB">
-				<img
-					id="img1B"
-					src={props.url}
-					alt="member"
-					width="160.75px"
-					height="160.75px"
-					style={{ borderRadius: "5px" }}
-				/>
-			</div>
-			<div className="infoB">
-				<div className="tittleB">
-					{props.tittle.length > 18
-						? props.tittle.substring(0, 18) + "..."
-						: props.tittle}
-				</div>
-				<div className="keteranganB">
-					{props.keterangan.length > 48
-						? props.keterangan.substring(0, 48) + "..."
-						: props.keterangan}
-				</div>
-			</div>
-		</div>
-	);
+      <div className="gambarB">
+        <img
+          id="img1B"
+          src={props.url}
+          alt="member"
+          width="160.75px"
+          height="160.75px"
+          style={{ borderRadius: "5px" }}
+        />
+      </div>
+      <div className="infoB">
+        <div className="tittleB">
+          {props.tittle.length > 18
+            ? props.tittle.substring(0, 18) + "..."
+            : props.tittle}
+        </div>
+        <div className="keteranganB">
+          {props.keterangan.length > 48
+            ? props.keterangan.substring(0, 48) + "..."
+            : props.keterangan}
+        </div>
+      </div>
+    </div>
+  );
 }
